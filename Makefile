ec9a4c90befb96fe42ac9c44eb20f6ac1150c7a4
Alê Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2023-04-13 00:59:05 +0100

    feat: Adds new files to libft.h and to the Makefile.

--- Makefile
+++ Makefile
@@ -1,7 +1,26 @@
 CC = cc
 CCFLAGS = -Wall -Wextra -Werror -c
 NAME = libft.a
-CFILES = ft_atoi.c ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c ft_isprint.c ft_isspace.c ft_strcpy.c ft_strdup.c ft_strlen.c ft_tolower.c ft_toupper.c
+CFILES = ft_memcpy.c\
+		ft_memset.c\
+		ft_memmove.c\
+		ft_bzero.c\
+		ft_atoi.c\
+		ft_isalnum.c\
+		ft_isalpha.c\
+		ft_isascii.c\
+		ft_isdigit.c\
+		ft_isprint.c\
+		ft_isspace.c\
+		ft_strlcpy.c\
+		ft_strlcat.c\
+		ft_strchr.c\
+		ft_strrchr.c\
+		ft_strncmp.c\
+		ft_strdup.c\
+		ft_strlen.c\
+		ft_tolower.c\
+		ft_toupper.c
 BFILES = ${CFILES:.c=.o}
 RM = rm -f
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: arabelo- <arabelo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 00:38:40 by arabelo-          #+#    #+#             */
-/*   Updated: 2023/04/12 14:01:37 by arabelo-         ###   ########.fr       */
+/*   Updated: 2023/04/12 22:10:18 by arabelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,15 @@ int		ft_isprint(int c);
 size_t	ft_strlen(const char *str);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-int		ft_strncmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_atoi(const char *str);
 char	*ft_strdup(const char *str);
 
