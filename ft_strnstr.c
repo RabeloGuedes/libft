63c2947a123aa9d2d3b7e06eb00ad84fdcb75ea9
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2023-04-13 20:59:40 +0100

    feat: Creates the ft_strnstr.c

---
+++ ft_strnstr.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arabelo- <arabelo-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 20:02:24 by arabelo-          #+#    #+#             */
+/*   Updated: 2023/04/13 20:12:06 by arabelo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	if (!len)
+		return (NULL);
+	if (!*little)
+		return ((char *)big);
+	return (NULL);
+}
